e31aed84ae936a047700457aec6ce541550094c9
Youssef Fadel <yousseffadel494@gmail.com>
2025-05-05 22:24:56 +0100

    Create Number Guessing Game.c

---
+++ Level 1 (Basic)/Number Guessing Game.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = rand() % 20 + 1; 
+    int user_number;
+
+    while(1)
+    {
+        printf("Enter a number: ");
+        scanf("%d",&user_number);
+    
+        if(user_number < a)
+           printf("guessing number is too low\n");
+        
+        else if(user_number > a)
+           printf("guessing number is too High\n");
+           
+        else
+        {
+           printf("Good guessing! You're correct");
+           break;
+        }
+    }
+      
+}
