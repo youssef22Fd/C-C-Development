45a742af95105be670750bd2132cbc2440d04726
Youssef Fadel <yousseffadel494@gmail.com>
2025-05-06 12:50:27 +0100

    Update Basic Calculator.c

--- Level 1 (Basic)/Basic Calculator.c
+++ Level 1 (Basic)/Basic Calculator.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
-
 int main(){
-
-  int8_t code;
+	
+        int8_t code;
 	int32_t n1, n2;
 	printf("Enter the code: ");
 	fflush(stdout);
@@ -15,31 +14,31 @@ int main(){
 	case 'a':
 	        printf("Enter the numbers: ");
         	scanf("%d %d", &n1, &n2);
-          printf("the add is %d ", n1 + n2);
-		      break;
+                printf("the add is %d ", n1 + n2);
+		break;
 
 	/* subtraction */
 	case 's':
-		      printf("Enter the numbers: ");
+	        printf("Enter the numbers: ");
         	scanf("%d %d", &n1, &n2);
-          printf("the sub is %d ", n1 - n2);
-		      break;
+                printf("the sub is %d ", n1 - n2);
+		break;
 
-    /* multiplication */
+        /* multiplication */
 	case 'm':
 	        printf("Enter the numbers: ");
         	scanf("%d %d", &n1, &n2);
-          printf("the mult is %d ", n1 * n2);
-			break;
+                printf("the mult is %d ", n1 * n2);
+		break;
 
-    /* division */
+        /* division */
 	case 'd':
-		     printf("Enter the numbers: ");
-         scanf("%d %d", &n1, &n2);
-		     if(n2==0)
-				       printf("the division by zero is undefined\n");
-         printf("the div is %d ", n1 / n2);
-		  	break;
+	       printf("Enter the numbers: ");
+               scanf("%d %d", &n1, &n2);
+		if(n2==0)
+	             printf("the division by zero is undefined\n");
+                printf("the div is %d ", n1 / n2);
+		break;
 
 	default:
 		printf("invalid operation\n");
