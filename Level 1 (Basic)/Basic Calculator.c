1b711ac3bbbc978afc32506716d598c7c93fa013
Youssef Fadel <yousseffadel494@gmail.com>
2025-05-08 12:45:56 +0100

    Update Basic Calculator.c

--- Level 1 (Basic)/Basic Calculator.c
+++ Level 1 (Basic)/Basic Calculator.c
@@ -6,40 +6,38 @@ int main(){
         int8_t code;
 	int32_t n1, n2;
 	
-	printf("Enter the numbers: ");
-        scanf("%d %d", &n1, &n2);
-
 	printf("Enter the code: ");
 	scanf("%c", &code);
+	
+	printf("Enter the numbers: ");
+        scanf("%d %d", &n1, &n2);
 	switch (code){
 		
 	    /* Addition */
 	    case '+':
-                printf("the add is %d ", n1 + n2);
-		break;
+	        	printf("the add of %d and %d is %d ",n1, n2, n1 + n2);
+		        break;
 
 	    /* subtraction */
 	    case '-':
-                printf("the sub is %d ", n1 - n2);
-		break;
+	        	printf("the sub of %d and %d is %d ",n1, n2, n1 - n2);
+		        break;
 
            /* multiplication */
 	   case '*':
-                printf("the mult is %d ", n1 * n2);
-		break;
+	        	printf("the mult of %d by %d is %d ",n1, n2, n1 * n2);
+		        break;
 
            /* division */
 	   case '/':
-		if(n2==0)
-		{
-	       printf("the division by zero is undefined\n");
-		   break;
-		}
-		printf("the div is %d ", n1 / n2);
-		break;
+	   
+	        	if(n2 == 0)
+	                 printf("the division by zero is undefined\n");
+	        	printf("the div of %d by %d is %d ",n1, n2, n1 / n2);
+		        break;
 
 	   default:
-		printf("invalid operation\n");
+		        printf("invalid operation\n");
 	}
 
 }
