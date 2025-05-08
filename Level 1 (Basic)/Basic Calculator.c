352369892e38573f611864d4dcbe486b0ff272d5
Youssef Fadel <yousseffadel494@gmail.com>
2025-05-08 17:48:19 +0100

    Update Basic Calculator.c

--- Level 1 (Basic)/Basic Calculator.c
+++ Level 1 (Basic)/Basic Calculator.c
@@ -1,3 +1,14 @@
+/*
+ * Basic Calculator
+ *
+ * This program allows the user to perform basic arithmetic operations
+ * (addition, subtraction, multiplication, and division) on two floating-point numbers.
+ * The user is prompted to enter an operation code ('+', '-', '*', '/') followed by two numbers.
+ * The program then performs the selected operation and displays the result.
+ * It also includes error handling for division by zero and invalid operation codes.
+ *
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -5,10 +16,8 @@ int main(){
 	
         int8_t code;
 	double n1, n2;
-	
 	printf("Enter the code: ");
 	scanf("%c", &code);
-	
 	printf("Enter the numbers: ");
         scanf("%lf %lf", &n1, &n2);
 	switch (code){
@@ -32,7 +41,11 @@ int main(){
 	   case '/':
 	   
 	        	if(n2 == 0)
+	        	{
 	                 printf("the division by zero is undefined\n");
+	                 break;
+	        	}
+	                 
 	        	printf("the div of %lf by %lf is %.2lf ",n1, n2, n1 / n2);
 		        break;
 
