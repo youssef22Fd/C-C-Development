2e16384f358f5e6b8e20fd2df23878be02054bd7
Youssef Fadel <yousseffadel494@gmail.com>
2025-05-08 17:50:42 +0100

    Update Number Guessing Game.c

--- Level 1 (Basic)/Number Guessing Game.c
+++ Level 1 (Basic)/Number Guessing Game.c
@@ -1,3 +1,17 @@
+/*
+ * Number Guessing Game
+ *
+ * This program generates a random number between 1 and 50 using the rand() function
+ * and asks the user to guess it. After each guess, it gives feedback:
+ *  - If the guess is lower than the target number, it informs the user it's too low.
+ *  - If the guess is higher than the target number, it informs the user it's too high.
+ *  - If the guess matches the target, it congratulates the user and exits.
+ *
+ * The random number generator is seeded with the current time using time(0)
+ * to ensure different outcomes on each execution.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
